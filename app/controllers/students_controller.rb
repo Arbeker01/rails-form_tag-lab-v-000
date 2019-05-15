4473b2d634ecc82eadb82dfa52cfa57280b5c639
Romeo Gono <romeogono1@aol.com>
2019-05-15 10:41:12 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,9 +12,9 @@ class StudentsController < ApplicationController
   
   end
   
-  def create 
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-     redirect_to new_student_path
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
 
 end
